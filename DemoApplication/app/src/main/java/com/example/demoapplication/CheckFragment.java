8e7a62102b79e2ab2c2d657246233b488f5dd1ee
ameng22 <amen1george2@gmail.com>
2023-01-17 11:28:34 +0530

    Swipe refresh added

--- DemoApplication/app/src/main/java/com/example/demoapplication/CheckFragment.java
+++ DemoApplication/app/src/main/java/com/example/demoapplication/CheckFragment.java
@@ -1,64 +1,85 @@
 package com.example.demoapplication;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CheckFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.fragment.app.Fragment;
+
+import java.util.Calendar;
+
 public class CheckFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Button dateBtn;
+    private TextView dateTxt;
+    private TextView currDate;
+    private Button timeBtn;
+    private TextView timeTxt;
 
     public CheckFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CheckFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CheckFragment newInstance(String param1, String param2) {
-        CheckFragment fragment = new CheckFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_check, container, false);
+        View view = inflater.inflate(R.layout.fragment_check, container, false);
+
+        dateTxt = (TextView) view.findViewById(R.id.display_date);
+        dateBtn = (Button) view.findViewById(R.id.date_btn);
+        currDate = (TextView) view.findViewById(R.id.textview_title);
+        timeBtn = (Button) view.findViewById(R.id.time_btn);
+        timeTxt = (TextView) view.findViewById(R.id.display_time);
+
+        timeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar c = Calendar.getInstance();
+
+                int hour = c.get(Calendar.HOUR_OF_DAY);
+                int min = c.get(Calendar.MINUTE);
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minOfDay) {
+                        timeTxt.setText( hourOfDay + ":" + minOfDay);
+                    }
+                },hour,min,false);
+                timePickerDialog.show();
+            }
+        });
+
+        dateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final Calendar c = Calendar.getInstance();
+
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+                currDate.setText("Date is: "+year+"-"+(month+1)+"-"+day);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int currYear, int monthOfYear, int dateOfMonth) {
+                        dateTxt.setText("Date is: "+dateOfMonth+"-"+(monthOfYear+1)+"-"+currYear);
+                    }
+                },year,month,day);
+
+                datePickerDialog.show();
+            }
+        });
+
+        return view;
     }
 }
--- DemoApplication/app/src/main/java/com/example/demoapplication/SaveFragment.java
+++ DemoApplication/app/src/main/java/com/example/demoapplication/SaveFragment.java
@@ -1,19 +1,18 @@
 package com.example.demoapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SaveFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
 public class SaveFragment extends Fragment {
+
+    private int counter = 0;
 
     public SaveFragment() {
         // Required empty public constructor
@@ -23,7 +22,26 @@ public class SaveFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_save, container, false);
+        View view = inflater.inflate(R.layout.fragment_save, container, false);
+        SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.refreshLayout);
+        TextView refreshText = (TextView) view.findViewById(R.id.save_text_view);
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+//                counter++;
+//                refreshText.setText("View is Refreshed " + counter + " times");
+//
+//                swipeRefreshLayout.setRefreshing(false);
+
+                Intent intent = new Intent(getContext(),CheckActivity.class);
+                startActivity(intent);
+
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+        return view;
     }
 
 
